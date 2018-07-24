4ebbb24618f3e7cc56945db3c1f1b0edeb1fa47e
papandas <hexoindia@gmail.com>
2018-07-24 22:30:45 +0530

    testing

--- contracts/Jobs.sol
+++ contracts/Jobs.sol
@@ -1,6 +1,24 @@
 pragma solidity ^0.4.23;
 
 contract Jobs {
+
+  enum AccountType {JobSeeker, Recruiter}
+
+  enum ExpertiseType {Engineer, GraphicDesigner, WebDeveloper, Driver, Manager}
+
+  struct Job{
+    bool active;
+    address addr;
+    AccountType accType;
+    string name;
+    ExpertiseType expertise;
+    uint256 year;
+    uint256 DoW;
+    uint256 salary;
+  }
+
+  mapping(address => Job) public jobs;
+
   address public owner;
 
   string public version = '0.0.1';
@@ -9,8 +27,11 @@ contract Jobs {
     owner = msg.sender;
   }
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
+  function SignUp(AccountType _accType, string _name, ExpertiseType _expertise, uint256 _year, uint256 _Dow, uint256 _salary) public {
+    require(jobs[msg.sender].active != true);
+
+    jobs[msg.sender] = Job(true, msg.sender, _accType, _name, _expertise, _year, _Dow, _salary);
+
   }
 
 }
--- test/Jobs.js
+++ test/Jobs.js
@@ -4,14 +4,38 @@ var Jobs = artifacts.require("./Jobs.sol");
 
 contract('Jobs Ethereum POC', function (accounts) {
 
+    const jobseeker1 = accounts[0];
+    const jobseeker2 = accounts[1];
+    const jobseeker3 = accounts[2];
+    const jobseeker4 = accounts[3];
+    const jobseeker5 = accounts[4];
+
+    const recruiter1 = accounts[5];
+    const recruiter2 = accounts[6];
+    const recruiter3 = accounts[7];
+    const recruiter4 = accounts[8];
+    const recruiter5 = accounts[9];
+
     it("Init project test.", function () {
         return Jobs.deployed().then(function (instance) {
             JobsInstance = instance;
             return JobsInstance.version();
         }).then((version) => {
-            console.log(version);
+            assert.equal(version, '0.0.1', 'Version is correct');
         });
-        
+    });
+
+
+    it("Add Accounts.", function () {
+        return Jobs.deployed().then(function (instance) {
+            JobsInstance = instance;
+            return JobsInstance.SignUp(0, "A", 1, 6, 5, 1000, {from: jobseeker1});
+        }).then((receipt) => {
+            console.log(receipt);
+            return JobsInstance.jobs(jobseeker1);
+        }).then((jobseeker)=>{
+            console.log(jobseeker);
+        });
     });
 
 });
